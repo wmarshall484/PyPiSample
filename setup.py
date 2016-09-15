9cd1b0c90bd9cdeda82abd57220f23de378d7e38
William C. Marshall <wcmarsha@us.ibm.com>
2016-09-15 16:17:01 -0400

    Updated version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pypisample',
   packages = ['pypisample'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A random test lib',
   author = 'Will Marshall',
   author_email = 'wmarshall484@gmail.com',
   url = 'https://github.com/wmarshall484/PyPiSample', # use the URL to the github repo
-  download_url = 'https://github.com/wmarshall484/PyPiSample/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/wmarshall484/PyPiSample/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'example'], # arbitrary keywords
   classifiers = [],
 )
