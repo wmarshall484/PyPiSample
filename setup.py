fadcfe762ac5a92b4765cd00c326ec5bfbc3ec92
William C. Marshall <wcmarsha@us.ibm.com>
2016-09-15 16:13:36 -0400

    First test

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pypisample',
+  packages = ['pypisample'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Will Marshall',
+  author_email = 'wmarshall484@gmail.com',
+  url = 'https://github.com/wmarshall484/PyPiSample', # use the URL to the github repo
+  download_url = 'https://github.com/wmarshall484/PyPiSample/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'example'], # arbitrary keywords
+  classifiers = [],
+)
